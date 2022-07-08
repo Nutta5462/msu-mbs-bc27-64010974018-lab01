b87615d81c199679fdb4a7f52021216e1e691303
Nutta5462 <108520720+Nutta5462@users.noreply.github.com>
2022-07-08 15:23:20 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,14 @@
+<html>
+ <head>
+  <title>PHP Test</title>
+ </head>
+ <body>
+ <?php echo '<p>Hello World</p>'; ?> 
+   <?php echo '<p>My name is Nuttapong</p>'; ?> 
+<?php echo '<p>My ID 64010974018 </p>'; ?> 
+     <?php echo '<p>Update-1</p>'; ?> 
+
+  
+  
+ </body>
+</html>
